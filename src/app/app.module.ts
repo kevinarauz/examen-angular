1b0c8b806c66f3a66236003cb24722f577f3754f
kevinarauz <kecin222294@gmail.com>
2023-04-23 21:50:55 -0500

    Se agrega componente pantalla examen

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PantallaExamenComponent } from './components/pantalla-examen/pantalla-examen/pantalla-examen.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PantallaExamenComponent
   ],
   imports: [
     BrowserModule
