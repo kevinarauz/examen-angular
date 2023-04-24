b716a3b890e70bbb5bc306277a3de202ebec46e6
kevinarauz <kecin222294@gmail.com>
2023-04-23 21:52:09 -0500

    Revert "Se agrega componente pantalla examen"
    This reverts commit 1b0c8b806c66f3a66236003cb24722f577f3754f.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { PantallaExamenComponent } from './components/pantalla-examen/pantalla-examen/pantalla-examen.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    PantallaExamenComponent
+    AppComponent
   ],
   imports: [
     BrowserModule
