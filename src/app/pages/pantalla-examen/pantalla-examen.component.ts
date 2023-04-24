6dd0a6ada7b22d28ff8bd177bf5b983998dac429
kevinarauz <kecin222294@gmail.com>
2023-04-23 22:11:25 -0500

    Se agrega componente pantalla examen

---
+++ src/app/pages/pantalla-examen/pantalla-examen.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pantalla-examen',
+  templateUrl: './pantalla-examen.component.html',
+  styleUrls: ['./pantalla-examen.component.css']
+})
+export class PantallaExamenComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
